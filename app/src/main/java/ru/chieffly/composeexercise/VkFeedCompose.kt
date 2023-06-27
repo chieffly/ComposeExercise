d3cee0f7a19ed114fbe7c205ec78a310d8904a59
Anton Bryantsev <chieffly@ya.ru>
2023-06-27 22:35:53 +0300

    VK post final

--- app/src/main/java/ru/chieffly/composeexercise/VkFeedCompose.kt
+++ app/src/main/java/ru/chieffly/composeexercise/VkFeedCompose.kt
@@ -2,15 +2,15 @@ package ru.chieffly.composeexercise
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -18,22 +18,29 @@ import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ru.chieffly.composeexercise.ui.theme.ComposeExerciseTheme
 
 /**
  *Created by Bryantsev Anton on 27.06.2023.
  **/
 
 @Composable
-fun VkFeedCard(){
-    Card(modifier = Modifier
-        .padding(4.dp),
+fun VkFeedCard() {
+    Card(
         shape = RoundedCornerShape(4.dp),
         border = BorderStroke(1.dp, Color.Gray)
     ) {
-        Column() {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+        ) {
             VkCardTopper()
-            Text(text = "текст поста",modifier =  Modifier.padding(4.dp))
-            Image(painter = ColorPainter(Color.Green), contentDescription = "", modifier =  Modifier.padding(4.dp).size(400.dp))
+            Text(text = "текст поста", modifier = Modifier.padding(4.dp))
+            Image(
+                painter = ColorPainter(Color.Green), contentDescription = "", modifier = Modifier
+                    .padding(4.dp)
+                    .size(400.dp)
+            )
             VkBottomPane()
         }
     }
@@ -41,38 +48,71 @@ fun VkFeedCard(){
 
 @Composable
 fun VkCardTopper() {
-    Row() {
-        Image(painter = ColorPainter(Color.Green), contentDescription = "", modifier = Modifier
-            .size(60.dp)
-            .padding(4.dp)
-            .clip(CircleShape))
-        Column(modifier = Modifier.padding(8.dp,0.dp)) {
-            Text(text = "Название группы")
-            Text(text = "15:20")
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = ColorPainter(Color.Green), contentDescription = "", modifier = Modifier
+                .size(60.dp)
+                .padding(4.dp)
+                .clip(CircleShape)
+        )
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+                .weight(1f)
+        ) {
+            Text(
+                text = "Название группы",
+                color = MaterialTheme.colors.onPrimary
+            )
+            Spacer(modifier = Modifier.size(4.dp))
+            Text(
+                text = "15:20",
+                color = MaterialTheme.colors.onSecondary
+            )
         }
+        Icon(painter = painterResource(R.drawable.baseline_more_vert_24), contentDescription = "", tint = MaterialTheme.colors.onSecondary)
     }
 }
 
 @Composable
 fun VkBottomPane() {
-    Row(modifier = Modifier.padding(8.dp)) {
-        IconWithCount("90", R.drawable.ic_eye_24)
-        IconWithCount("206", R.drawable.baseline_share_24)
-        IconWithCount("11", R.drawable.baseline_message_24)
-        IconWithCount("458", R.drawable.baseline_favorite_border_24)
+    Row {
+        Row(modifier = Modifier.weight(1f)) {
+            IconWithCount(R.drawable.ic_eye_24, "90")
+        }
+        Row(modifier = Modifier.weight(1f), horizontalArrangement = Arrangement.SpaceBetween) {
+            IconWithCount(R.drawable.baseline_share_24, "206")
+            IconWithCount(R.drawable.baseline_message_24, "11")
+            IconWithCount(R.drawable.baseline_favorite_border_24, "458")
+        }
     }
 }
 
 @Composable
-fun IconWithCount(count: String, drawableId: Int){
-    Row() {
-        Text(text = count,modifier = Modifier.padding(4.dp))
-        Image(painter = painterResource(drawableId), contentDescription = "")
+fun IconWithCount(drawableId: Int, count: String) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(painter = painterResource(drawableId), contentDescription = "", tint = MaterialTheme.colors.onSecondary)
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(text = count, color = MaterialTheme.colors.onSecondary)
     }
 }
 
 @Preview
 @Composable
-fun PreviewVK() {
-    VkFeedCard()
+private fun PreviewVKLight() {
+    ComposeExerciseTheme(darkTheme = false) {
+        VkFeedCard()
+
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewVKDark() {
+    ComposeExerciseTheme(darkTheme = true) {
+        VkFeedCard()
+    }
 }
--- app/src/main/java/ru/chieffly/composeexercise/ui/theme/Theme.kt
+++ app/src/main/java/ru/chieffly/composeexercise/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package ru.chieffly.composeexercise.ui.theme
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
@@ -7,28 +8,30 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
+@SuppressLint("ConflictingOnColor")
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200,
+    primary = Black900,
+    primaryVariant = Black900,
+    secondary = Black900,
 
     background = Color.Black,
     surface = Color.Black,
-    onPrimary = Color.Black,
-    onSecondary = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Black500,
     onBackground = Color.White,
     onSurface = Color.White
 )
 
+@SuppressLint("ConflictingOnColor")
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200,
+    primary = Color.White,
+    primaryVariant = Color.White,
+    secondary = Color.White,
 
     background = Color.White,
     surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
+    onPrimary = Black900,
+    onSecondary = Black500,
     onBackground = Color.Black,
     onSurface = Color.Black
 
